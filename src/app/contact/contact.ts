7f681a6b5d9accc18bcdaf93b6478278ef0b1753
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2026-03-29 17:50:00 +0200

    Added contact page

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
